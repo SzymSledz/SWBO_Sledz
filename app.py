b6f310fbde9bb42be168f5a7f89cc937c6a9c462
SzymSledz <szymonsledz23@gmail.com>
2023-12-23 17:15:06 +0100

    Added sesions, basic user page and improved login

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
+from sqlalchemy import *
 
 app = Flask(__name__)
-
+app.secret_key = "@$FA@Tzw$t"
+app.permanent_session_lifetime = timedelta(hours = 2)
 @app.route("/")
 def main_page():
     return render_template("index.html")
@@ -9,14 +12,27 @@ def main_page():
 @app.route("/login", methods=["GET", "POST"])
 def login_page():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["login"]
-        return redirect(url_for("user_page", usr=user))
+        session["user"] = user
+        return redirect(url_for("user_page"))
     else:
+        if("user" in session):
+            return redirect(url_for("user_page"))
         return render_template("login.html")
 
-@app.route("/<usr>")
-def user_page(usr):
-    return f"<h1>Wellcome {usr}</h1>"
+@app.route("/user")
+def user_page():
+    if "user" in session:
+        usr = session["user"]
+        return render_template("user.html")
+    else:
+        return redirect(url_for("login_page"))
+
+@app.route("/logout")
+def logout_page():
+    session.pop("user", None)
+    return redirect(url_for("login_page"))
 
 if __name__ == '__main__':
     app.run(debug = True)
