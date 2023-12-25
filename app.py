aac002c5f4e6c56253ee94650c5f175a72c5b14d
SzymSledz <szymonsledz23@gmail.com>
2023-12-25 22:02:10 +0100

    Made login and sign up forms fully functional

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template, request, session
+from flask import Flask, redirect, url_for, render_template, request, session, flash
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
@@ -26,6 +26,10 @@ def main_page():
 
 @app.route("/login", methods=["GET", "POST"])
 def login_page():
+    if "logged_in" in session:
+        return redirect(url_for("user_page"))
+    if "login_failed" not in session:
+        session["login_failed"] = False
     if request.method == "POST":
         session.permanent = True
         user_login = request.form["login"]
@@ -33,24 +37,31 @@ def login_page():
         session["login"] = user_login
         session["password"] = user_password
 
-        session["logged_in"] = True # if login is succesfull
-
         user_in_db = users.query.filter_by(login=user_login).first()
-        if user_in_db:
-            session["login"] = user_in_db.login
-        else:
-            usr = users(user_login, user_password)
-            db.session.add(usr)
-            db.session.commit()
 
-        return redirect(url_for("user_page"))
-    else:
-        if "logged_in" in session:
-            return redirect(url_for("user_page"))
+        if user_in_db:
+            user_in_db_password = users.query.filter_by(login=user_login).first().password
+
+            if user_password == user_in_db_password:
+                session["logged_in"] = True  # if login is succesfull
+                return redirect(url_for("user_page"))
+            else:
+                #failed to login
+                session["login_failed"] = True
+                return redirect(url_for("login_page", login_failed=session["login_failed"]))
         else:
-            login = ''
-            password = ''
-        return render_template("login.html", login=login, password=password)
+            # failed to login user not found
+            session["login_failed"] = True
+            return redirect(url_for("login_page", login_failed=session["login_failed"]))
+    else:
+        login = ''
+        password = ''
+
+        if session["login_failed"] == True: # set login_failed = False in order to display error only once
+            session["login_failed"] = False
+            return render_template("login.html", login=login, password=password, login_failed=True)
+        return render_template("login.html", login=login, password=password, login_failed=False)
+
 
 @app.route("/user", methods=["GET", "POST"])
 def user_page():
@@ -75,13 +86,46 @@ def logout_page():
     session.pop("login", None)
     session.pop("password", None)
     session.pop("logged_in", None)
+    session.pop("login_failed", None)
     return redirect(url_for("login_page"))
 
 @app.route("/sign_up", methods=["GET", "POST"])
 def sign_up_page():
     if "logged_in" in session:
-        return redirect(url_for("login_page"))
-    return render_template("sign_up.html")
+        return redirect(url_for("user_page"))
+    if request.method == "POST":
+        session.permanent = True
+        user_login = request.form["login"]
+        user_password = request.form["password"]
+        user_password_repeat = request.form["passwordRepeat"]
+
+        session["login"] = user_login
+        session["password"] = user_password
+
+        user_in_db = users.query.filter_by(login=user_login).first()
+
+        if user_in_db: #user login allready taken
+            flash("Ta nazwa użytkownika jest już zajęta", "warning")
+            return redirect(url_for("sign_up_page", login=session["login"]))
+        else:
+            if user_password != user_password_repeat:
+                flash("Hasło i jego powtrzórzenie nie są identyczne", "warning")
+                return redirect(url_for("sign_up_page"))
+            if user_password == '':
+                flash("Hasło nie może być puste", "warning")
+                return redirect(url_for("sign_up_page"))
+            new_user = users(user_login, user_password)
+            db.session.add(new_user)
+            db.session.commit()
+            session["logged_in"] = True  # if login is succesfull
+            return redirect(url_for("user_page"))
+    else:
+        if "logged_in" in session:
+            return redirect(url_for("user_page"))
+        else:
+            if "login" in session:
+                return render_template("sign_up.html", login=session["login"])
+            return render_template("sign_up.html", login="")
 
 if __name__ == '__main__':
     db.create_all()
